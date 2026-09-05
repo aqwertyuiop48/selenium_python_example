5c2a0fb85a09d7780fedcb04de9950543fa4ffa5
Sreedhar_K <aqwertyuiop48@hotmail.com>
2026-09-05 11:04:34 +0530

    Change search button locator to CSS selector
    Updated the locator for the search button to use CSS selector.

--- data/locators.py
+++ data/locators.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class SearchPageLocators:
-    #SEARCH_INPUT = (By.ID, "searchbox_input")
     SEARCH_INPUT = (By.NAME, "q")
-    SEARCH_BUTTON = (By.XPATH, "//*[@id='searchbox_homepage']//*[@type='submit']")
+    SEARCH_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
     RESULTS = (By.XPATH, "//*[@data-testid='mainline']//*[@data-testid='result']")
