aef5ca8fc359e5fbd3d93753837a4de6953bc7bb
Sreedhar_K <aqwertyuiop48@hotmail.com>
2026-09-05 11:27:15 +0530

    Import By and Keys from selenium.webdriver

--- pages/search_page.py
+++ pages/search_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from pages.base_page import BasePage
 from data.locators import SearchPageLocators
 
