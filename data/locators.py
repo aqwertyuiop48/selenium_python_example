e5c9f52dd8c42b34f385a5960ddfc7029cfec1cd
Sreedhar_K <aqwertyuiop48@hotmail.com>
2026-09-05 10:56:46 +0530

    Update locators.py

--- data/locators.py
+++ data/locators.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class SearchPageLocators:
-    SEARCH_INPUT = (By.ID, "searchbox_input")
+    #SEARCH_INPUT = (By.ID, "searchbox_input")
+    SEARCH_INPUT = (By.NAME, "q")
     SEARCH_BUTTON = (By.XPATH, "//*[@id='searchbox_homepage']//*[@type='submit']")
     RESULTS = (By.XPATH, "//*[@data-testid='mainline']//*[@data-testid='result']")
