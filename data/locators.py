c159c98ca4bf6c4272b0c9e1a43a21143672a94d
Sreedhar_K <aqwertyuiop48@hotmail.com>
2026-09-05 11:10:08 +0530

    Update locators.py

--- data/locators.py
+++ data/locators.py
@@ -3,5 +3,5 @@ from selenium.webdriver.common.by import By
 
 class SearchPageLocators:
     SEARCH_INPUT = (By.NAME, "q")
-    SEARCH_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
+    SEARCH_BUTTON = (By.XPATH, "//button[@aria-label='Search']")
     RESULTS = (By.XPATH, "//*[@data-testid='mainline']//*[@data-testid='result']")
